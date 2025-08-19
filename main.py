61bfbd6f04d73865a5037b0a416d5ba18ce34fbd
Jineon Baek <jineon@umich.edu>
2025-08-19 14:20:54 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 import os
 import fire
 import lib.erdos as erdos
+import itertools
 from lib.util import binom
+
+def subsets(n, k):
+    return itertools.combinations(list(range(n)), k)
 
 etv = {
     "": erdos.etv,
@@ -12,12 +16,22 @@ etv = {
     "ax4_interior": erdos.etv_ax4_interior,
     "ax5": erdos.etv_ax5,
     "semisimple": erdos.etv_semisimple,
+    "restrict": erdos.etv_signotope,
 }
 
 def main(n, a, b, N, filename=None, order=""):
     inst = etv[order](n, a, b, N)
     if not filename:
         filename = f"etv_{n}_{a}_{b}_{N}"
+    if order == "restrict":
+        v = inst.v
+        for i, j, k, l in subsets(N, 4):
+            p = v[(i, j, k)]
+            q = v[(i, j, l)]
+            r = v[(i, k, l)]
+            s = v[(j, k, l)]
+            inst.add_implies(p & q & ~s, r)
+
     if inst.solve(filename):
         print("Solution found")
         sol_filename = filename + ".color"
